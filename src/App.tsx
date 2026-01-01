320b26adbc345950789e758a2ec7e8e11167ae4e
Rahul Pal <usefulltech1819@gmail.com>
2026-01-02 01:45:26 +0530

    Add function Add  to Queue and component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,22 @@
+import { useState } from "react";
 import QueueForm from "./components/QueueForm";
+import type { NewCustomer, QueueItem } from "./types/Queue";
 
 function App() {
+    const [queue, setQueue] = useState<QueueItem[]>([]);
+
+    const addToQueue = (customer: NewCustomer) => {
+        setQueue((prev) => [
+            ...prev, 
+            {
+                ...customer, 
+                id: Date.now(), 
+                status: "waiting"
+            }
+        ]);
+    };
+
+   
     return (
         <div className="min-h-screen bg-[#1a1a1a] m-auto text-center p-8 text-white">
             <div className="m-8 text-center">
@@ -11,8 +27,8 @@ function App() {
                     Manage your customers efficently
                 </p>
             </div>
-            <main>
-                
+            <main className="grid gap-8 grid-cols-[350px_1fr]">
+                <QueueForm onAdd={addToQueue} />
             </main>
         </div>
     );
