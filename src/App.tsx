f3ab94d0a878da082cf7f84cb3e465563ab1d005
Rahul Pal <usefulltech1819@gmail.com>
2026-01-01 22:52:57 +0530

    Add Main Heading and paragraph

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import QueueForm from "./components/QueueForm";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <div className="min-h-screen bg-[#1a1a1a] m-auto text-center p-8 text-white">
+            <div className="m-8 text-center">
+                <h1 className="text-[#646cff] text-3xl font-bold">
+                    Queue Managment System
+                </h1>
+                <p className="text-[#888888] mt-2">
+                    Manage your customers efficently
+                </p>
+            </div>
+            <main>
+                
+            </main>
+        </div>
+    );
 }
 
-export default App
+export default App;
